db2800e99e62a002f7e4a86e4f175df28e5e566d
sarak1234 <sarakycyku123@gmail.com>
2025-12-06 17:25:51 +0100

    stilizimin ne style.css edhe krijimi i komponentes te navbarit

--- html/js/nav.js
+++ html/js/nav.js
@@ -1,19 +1,30 @@
 class PharmaNavbar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-       <nav class="navbar bg-light">
-  <div class="container">
-    <img src="#" alt="Logo" width="35" height="35">
-    
-    <h5 class="mx-auto mb-0">Pharma Lirë</h5>
-    
-    <div>
-      <a href="#" class="text-dark mx-2 text-decoration-none">Home</a>
-      <a href="#" class="text-dark mx-2 text-decoration-none">Page</a>
-      <a href="#" class="text-dark mx-2 text-decoration-none">Page</a>
-    </div>
-  </div>
-</nav>
+       <nav class="navbar navbar-expand-lg navbar-custom px-4 py-3">
+        <a class="brand-green" href="#">
+            <img src="img/logo.png" alt="logo" style="height:80px;">
+            <span>PharmaLirë</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
+            aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span style="font-size:20px;color:#0b7d50">☰</span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="mainNav">
+            <ul class="navbar-nav align-items-center">
+                <li class="nav-item"><a class="nav-link custom" href="index.html" data-translate="home">Home</a></li>
+                <li class="nav-item"><a class="nav-link custom" href="#" data-translate="compare">Compare Prices</a>
+                </li>
+                <li class="nav-item"><a class="nav-link custom" href="#" data-translate="pharmacies">Pharmacies</a></li>
+                <li class="nav-item"><a class="nav-link custom" href="#" data-translate="about">About</a></li>
+                <li class="nav-item"><a class="nav-link custom" href="#" data-translate="contact">Contact</a></li>
+                <li class="nav-item ms-3"> <button class="btn btn-sm btn-login me-2"
+                        onclick="location.href='login.html'">Login</button> </li>
+                <li class="nav-item ms-3"> <button class="btn btn-sm btn-signup"
+                        onclick="location.href='signup.html'">Sign Up</button> </li>
+            </ul>
+        </div>
+    </nav>
     `;
   }
 }
