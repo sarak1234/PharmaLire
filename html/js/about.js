e2fb81b73857bdb3394b20d24c70839a3489696b
sarak1234 <sarakycyku123@gmail.com>
2025-12-14 01:53:42 +0100

    Added 2 new charts one  for price comparison andthe other for free usage

--- html/js/about.js
+++ html/js/about.js
@@ -1,11 +1,10 @@
-;window.addEventListener('DOMContentLoaded', () => {
-    // Chart për transparencën e çmimeve
+window.addEventListener('DOMContentLoaded', () => {
     const chartTransparency = new Chart("chartTransparency", {
         type: "pie",
         data: {
             labels: ["Shumë e rëndësishme", "E rëndësishme", "Jo shumë/aspak"],
             datasets: [{
-                backgroundColor: ["#0d6efd", "#17a2b8", "#ffc107"],
+                backgroundColor: ["#3366cc", "#ff9900", "#dc3811"],
                 data: [67.4, 27.9, 4.7]
             }]
         },
@@ -19,13 +18,12 @@
         }
     });
 
-    // Chart për vështirësinë në gjetjen e çmimeve
     const chartDifficulty = new Chart("chartDifficulty", {
         type: "pie",
         data: {
             labels: ["Po", "Jo"],
             datasets: [{
-                backgroundColor: ["#28a745", "#dc3545"],
+                backgroundColor: ["#3366cc", "#dc3811"],
                 data: [90.7, 9.3]
             }]
         },
@@ -38,4 +36,52 @@
             maintainAspectRatio: true
         }
     });
+    
+ const chartCompare = new Chart("chartCompare", {
+        type: "pie",
+        data: {
+            labels: ["Po", "Ndoshta"],
+            datasets: [{
+                backgroundColor: ["#3366cc", "#dc3545"],
+                data: [87.7, 12.3]
+            }]
+        },
+        options: {
+            plugins: {
+                legend: { position: 'bottom', onClick: null },
+                tooltip: { enabled: true }
+            },
+            responsive: true,
+            maintainAspectRatio: true
+        }
+    });
+
+
+    const chartFree = new Chart("chartFree", {
+        type: "pie",
+        data: {
+            labels: ["Po", "Ndoshta", "Jo"],
+            datasets: [{
+                backgroundColor: ["#3366cc", "#ff9900", "#dc3811"],
+                data: [86.2, 10.8, 3.0]
+            }]
+        },
+        options: {
+            plugins: {
+                legend: { position: 'bottom', onClick: null },
+                tooltip: { enabled: true }
+            },
+            responsive: true,
+            maintainAspectRatio: true
+        }
+    });
 });
+
+
+
+
+
+//kuqe ----#dc3811
+
+//kalter--#3366cc
+//orange--#ff9900
